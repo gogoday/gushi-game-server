3cb22352f5c4029301c235f9856ba347200fa0d7
sampsonwang <sampsonwang@tencent.com>
2021-05-26 15:33:19 +0800

    测试下载文件函数

---
+++ functions/test-file/index.js
@@ -0,0 +1,19 @@
+const tcb = require("@cloudbase/node-sdk");
+
+// const app = tcb.init({
+//   env: "hello-cloudbase-1gjrribi96ea328d"
+// });
+
+const app = tcb.init();
+
+exports.main = async (event, context) => {
+  const content = await app
+  .downloadFile({
+    fileID: "cloud://hello-cloudbase-1gjrribi96ea328d.6865-hello-cloudbase-1gjrribi96ea328d-1251036730/json/poet.song.1000.json"
+  })
+  .then((res) => {
+    // fileContent 类型为 Buffer
+    return res.fileContent.toString('utf-8');
+  });
+  return content;
+}
