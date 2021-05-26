7012d77eed9453e1cb9ab0d795a621d0a9e2c7a0
sampsonwang <sampsonwang@tencent.com>
2021-05-26 11:48:20 +0800

    查询db check 答案是否正确

--- functions/gushi-game-check/index.js
+++ functions/gushi-game-check/index.js
@@ -1,14 +1,42 @@
+const cloudbase = require('@cloudbase/node-sdk')
 
+const app = cloudbase.init({})
+
+// 1. 获取数据库引用
+var db = app.database()
 
 exports.main = async (event, context) => {
-  const { body } = event;
-  const selectWords = JSON.parse(body);
-  console.log(selectWords);
-  // 读数据库 校验数据
+  if (!event.body) {
+    return { ret: 2 }
+  }
+  console.log(event.body)
+  const { userId, select, create_time } = JSON.parse(event.body);
+  if (!userId || !create_time || !select) {
+    return { ret: 3}
+  }
+  console.log(JSON.parse(event.body))
+  const res = await db.collection('gushi')
+    .where({
+      userId,
+      create_time,
+    })
+    .get();
+  const data = res.root && res.root.data;
+  if (!data) {
+    return {
+      ret: 5,
+    }
+  }
+  const { deleteWolds } = data[0];
+  for (let i = 0, l = deleteWolds.length; i < l; i++) {
+    if (deleteWolds[i] !== select[i]) {
+      return {
+        ret: 4
+      }
+    }
+  }
 
-  return true ? {
+  return {
     ret: 0,
-  } : {
-    ret: 1
   }
 }
--- functions/gushi-game-check/index.test.js
+++ functions/gushi-game-check/index.test.js
@@ -6,7 +6,10 @@ const mySer = require('./index');
 
 describe('main', () => {
   it('should get a right word', async () => {
-    const result = await mySer.main();
+    const event = {
+      body: JSON.stringify({"select":["入","至","光","坐"],"userId":"1621996039519-0.9863158576881599","create_time":1621998250455})
+    }
+    const result = await mySer.main(event);
     console.log(result)
     expect(result).to.include.keys('ret');
   })
